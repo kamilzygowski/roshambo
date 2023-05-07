42ab8162706b133d85c9e3755e3d1ad7f15c5c57
Kamil Żygowski <kzygowski7@gmail.com>
2023-05-07 23:35:05 +0200

    socket server cors

--- server/main.go
+++ server/main.go
@@ -6,18 +6,27 @@ import (
 	"net/http"
 
 	socketio "github.com/googollee/go-socket.io"
+	"github.com/googollee/go-socket.io/engineio"
+	"github.com/googollee/go-socket.io/engineio/transport"
+	"github.com/googollee/go-socket.io/engineio/transport/polling"
+	"github.com/googollee/go-socket.io/engineio/transport/websocket"
 )
-
-func hello(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Server is online"))
-}
 
 var allowOriginFunc = func(r *http.Request) bool {
 	return true
 }
 
 func main() {
-	server := socketio.NewServer(nil)
+	server := socketio.NewServer(&engineio.Options{
+		Transports: []transport.Transport{
+			&polling.Transport{
+				CheckOrigin: allowOriginFunc,
+			},
+			&websocket.Transport{
+				CheckOrigin: allowOriginFunc,
+			},
+		},
+	})
 
 	server.OnConnect("/socket.io/", func(s socketio.Conn) error {
 		s.SetContext("")
