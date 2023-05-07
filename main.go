58b7174d06c8a141a568d36c86ad6e236e574515
Kamil Żygowski <kzygowski7@gmail.com>
2023-05-07 20:39:36 +0200

    socket + http serving

--- main.go
+++ main.go
@@ -1,15 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	socketio "github.com/googollee/go-socket.io"
+)
 
 func main() {
+	server := socketio.NewServer(nil)
+
+	server.OnConnect("/", func(s socketio.Conn) error {
+		s.SetContext("")
+		fmt.Println("Connected: ", s.ID())
+		return nil
+	})
+
 	allPlayers := []player{}
 	fmt.Println(allPlayers)
-	createNewPlayer(player{5, 5, "Newplayer", 1}, &allPlayers)
+	createNewPlayer(player{0, 5, 5, "Newplayer", 1}, &allPlayers)
+	createNewPlayer(player{0, 10, 10, "SecondOne", 2}, &allPlayers)
 	fmt.Println(allPlayers)
-	fmt.Println("Running")
-	for {
-		fmt.Println(allPlayers)
-		allPlayers[0].move()
-	}
+	go server.Serve()
+	defer server.Close()
+
+	http.Handle("/socket.io", server)
+	log.Println("Serving at localhost:8000...")
+	log.Fatal(http.ListenAndServe(":8000", nil))
+	/*for {
+		//fmt.Println(allPlayers)
+		//allPlayers[0].move(0)
+	}*/
 }
